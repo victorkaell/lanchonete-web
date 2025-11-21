512ffbb22b0f53cc17bd45162b0bf04b5010e4b3
Victor Kaell <victorkaell13@gmail.com>
2025-11-21 12:48:55 -0300

    Importado o AdicionalRepository

--- src/main/java/io/github/victorkaell/lanchonete/controllers/MenuController.java
+++ src/main/java/io/github/victorkaell/lanchonete/controllers/MenuController.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import io.github.victorkaell.lanchonete.models.Adicional;
 import io.github.victorkaell.lanchonete.models.Produto;
+import io.github.victorkaell.lanchonete.repositories.AdicionalRepository;
 import io.github.victorkaell.lanchonete.repositories.ProdutoRepository;
 
 @Controller
