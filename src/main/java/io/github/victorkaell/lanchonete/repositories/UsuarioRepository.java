eeea8a27b2a820c01c642ba11d80330a547e55b3
Victor Kaell <victorkaell13@gmail.com>
2025-12-15 15:57:22 -0300

    Atualizado método do UsuarioRepository

--- src/main/java/io/github/victorkaell/lanchonete/repositories/UsuarioRepository.java
+++ src/main/java/io/github/victorkaell/lanchonete/repositories/UsuarioRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import io.github.victorkaell.lanchonete.models.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-	Usuario findByNomeAndSenha(String nome, String senha);
+	Usuario findByNome(String nome);
 }
