5205290fa36b1df42ff58688700f3a132b5d0d6e
Victor Kaell <victorkaell13@gmail.com>
2025-12-15 23:53:19 -0300

    Removido código obsoleto

--- src/main/java/io/github/victorkaell/lanchonete/controllers/MenuController.java
+++ src/main/java/io/github/victorkaell/lanchonete/controllers/MenuController.java
@@ -7,9 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import io.github.victorkaell.lanchonete.models.Adicional;
 import io.github.victorkaell.lanchonete.models.Produto;
-import io.github.victorkaell.lanchonete.repositories.AdicionalRepository;
 import io.github.victorkaell.lanchonete.repositories.ProdutoRepository;
 
 @Controller
@@ -18,18 +16,13 @@ public class MenuController {
 	@Autowired
 	private ProdutoRepository pr;
 
-	@Autowired
-	private AdicionalRepository ar;
-
 	@GetMapping("/home")
 	public ModelAndView inicio() {
 		ModelAndView mv = new ModelAndView();
 
 		List<Produto> produtos = pr.findAll();
-		List<Adicional> adicionais = ar.findAll();
 
 		mv.addObject("produtos", produtos);
-		mv.addObject("adicionais", adicionais);
 
 		mv.setViewName("home");
 
