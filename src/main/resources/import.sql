bd612561d459d1897585a44d28da65cf64399c38
Victor Kaell <victorkaell13@gmail.com>
2026-03-28 00:16:25 -0300

    Adicionados dados para testes

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO usuario (id, cargo, nome, senha) VALUES (1, 'CLIENTE', 'CLIENTE', '$2a$10$xgLw0MiGb0.hePfYTIipOe0z.YLNSPIwQ1wKO/X837VzTLbunQZNK');
+INSERT INTO usuario (id, cargo, nome, senha) VALUES (2, 'ADMIN', 'ADMIN', '$2a$10$xgLw0MiGb0.hePfYTIipOe0z.YLNSPIwQ1wKO/X837VzTLbunQZNK');
+INSERT INTO usuario (id, cargo, nome, senha) VALUES (3, 'COZINHA', 'COZINHA', '$2a$10$xgLw0MiGb0.hePfYTIipOe0z.YLNSPIwQ1wKO/X837VzTLbunQZNK');
+
+INSERT INTO produto (id, nome, descricao, preco, esta_disponivel, imagemurl) VALUES (1, 'Hambúrguer Clássico', 'Pão, carne bovina 160g, queijo cheddar e salada.', 25.50, 1, 'https://imagem.com');
+INSERT INTO produto (id, nome, descricao, preco, esta_disponivel, imagemurl) VALUES (2, 'Refrigerante Cola', 'Lata 350ml bem gelada.', 6.00, 1, 'https://imagem.com');
+INSERT INTO produto (id, nome, descricao, preco, esta_disponivel, imagemurl) VALUES (3, 'Porção de Batatas', 'Batatas rústicas fritas com alecrim.', 15.00, 1, 'https://imagem.com');
